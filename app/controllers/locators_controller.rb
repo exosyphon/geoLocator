171ef5eb4b1badd3e754c9dd3d57c3ec8e5c4906
exosyphon <exosyphon@yahoo.com>
2014-07-01 23:26:28 -0600

    update controller

--- app/controllers/locators_controller.rb
+++ app/controllers/locators_controller.rb
@@ -2,7 +2,10 @@ require 'simple_geolocation'
 
 class LocatorsController < ApplicationController
   def index
-    ip = request.remote_ip.present? || '50.155.177.142'
+    ip = request.remote_ip
+    if ip == '127.0.0.1'
+      ip = '50.155.177.142'
+    end
     @location = SimpleGeolocation::Geocoder.new(ip).geocode!
   end
 end
