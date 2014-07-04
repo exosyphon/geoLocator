60e2b0d3ef48bf34c9c2a77ada37fbcbbc964748
exosyphon <exosyphon@yahoo.com>
2014-07-03 22:22:51 -0600

    testing mobile

--- app/controllers/locators_controller.rb
+++ app/controllers/locators_controller.rb
@@ -4,12 +4,12 @@ class LocatorsController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => :new_request
 
   def index
-    puts request.env
-    @one = " stuff #{request.env['REMOTE_ADDR']}"
-    @two = " stuff #{request.env['HTTP_X_FORWARDED_FOR']}"
-    @three =  " stuff #{request.env['HTTP_CLIENT_IP']}"
-    @four = "sadf #{request.env['REMOTE_HOST']}"
-    @five =  " stuff #{request.env['action_dispatch.remote_ip']}"
+    @one = " stuff #{request.env['HTTP_FORWARDED']}"
+    @two = " sadf #{request.env['REMOTE_HOST']}"
+    @three =  " stuff #{request.env['HTTP_FORWARDED_FOR']}"
+    @four =  " stuff #{request.env['HTTP_X_CLUSTER_CLIENT_IP']}"
+    @five =  " stuff #{request.env['HTTP_X_FORWARDED']}"
+
     ip = request.remote_ip
     if ip == '127.0.0.1'
       ip = '74.125.113.104'
